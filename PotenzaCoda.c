7f75676b2e71db1d2f1974696b07f9aeccc67766
tonyiovino <antonioiovino2004tony@gmail.com>
2021-03-13 15:06:28 +0100

    Name variable and remove initialization's pow variable

--- PotenzaCoda.c
+++ PotenzaCoda.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
-int potenza_helper(int a, int b, int parziale);
-int potenza(int a, int b);
+int potenza_helper(int a, int x, int parziale);
+int potenza(int a, int x);
 
 int main(void){
 
-	int a, b;
-	int pow = 0;
+	int a, x;
+	int pow;
 
 	printf("Inserisci la potenza (es. 3^2): ");
-	scanf("%d^%d", &a, &b);
+	scanf("%d^%d", &a, &x);
 
-	pow = potenza(a, b);
-	printf("%d^%d = %d\n", a, b, pow);
+	pow = potenza(a, x);
+	printf("%d^%d = %d\n", a, x, pow);
 
 	return 0;
 }
 
-int potenza(int a, int b){
-	return potenza_helper(a, b, 1);
+int potenza(int a, int x){
+	return potenza_helper(a, x, 1);
 }
 
-int potenza_helper(int a, int b, int parziale){
+int potenza_helper(int a, int x, int parziale){
 
-	if (b == 1) return parziale * a;
+	if (x == 1) return parziale * a;
 
-	return potenza_helper(a, b-1, parziale*a);
+	return potenza_helper(a, x-1, parziale*a);
 }
