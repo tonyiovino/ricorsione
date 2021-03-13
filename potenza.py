8824a00617446259d74c7589faef06d80154bf9b
tonyiovino <antonioiovino2004tony@gmail.com>
2021-03-13 16:13:22 +0100

    Fix while

--- potenza.py
+++ potenza.py
@@ -1,18 +1,17 @@
 def potenza(b, e):
-	if (e == 1):
+	if e == 1:
 		return b
-	elif (e == 0):
+	elif e == 0:
 		return 1
 
 	return b * potenza(b, e-1)
 
 # main
-e = 0
+e = -1
 while e < 0:
 	b, e = input("Inserisci la potenza (es. 3^2): ").split('^')
-
-b = int(b)
-e = int(e)
+	b = int(b)
+	e = int(e)
 
 power = potenza(b, e)
 
